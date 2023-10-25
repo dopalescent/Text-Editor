261894c2020e3ad174b7363155a0c333bb9bf857
dopalescent <skudlgre000@gmail.com>
2023-10-25 16:01:57 -0400

    implemented asset caching in src-sw.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
@@ -28,7 +27,6 @@ export const putDb = async (content) => {
   }
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
