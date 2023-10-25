919094f328eb2af2ff0b48691ee9b85ee5ade98d
dopalescent <skudlgre000@gmail.com>
2023-10-25 15:11:50 -0400

    added indexedDB methods to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content: content });
+  const result = await request;
+  if (result) {
+    console.log('Data saved to the database', result);
+    return result;
+  } else {
+    console.error('putDb not implemented')
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  if (result) {
+    console.log('result.value', result);
+    return result;
+  } else {
+    console.error('getDb not implemented');
+  };
+};
 
 initdb();
