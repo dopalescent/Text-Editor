4f2e06e3be76431235fbe87dc00b9a5f3a9ef62d
dopalescent <skudlgre000@gmail.com>
2023-10-25 16:39:04 -0400

    troubleshooting, text content now saves and loads properly

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,11 +17,9 @@ export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({ id: id, value: content });
+  const request = store.put({ id: 1, value: content });
   const result = await request;
   console.log('Data saved to the database', result);
-  return result;
-
 };
 
 export const getDb = async () => {
@@ -31,8 +29,8 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('Data read from the database', result);
+  return result?.value;
 };
 
 initdb();
